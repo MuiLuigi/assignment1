0af2bab2cec4780637fc8a7c408856bda4f608f6
Maruigi <fahadmui123@gmail.com>
2025-06-01 11:34:57 -0400

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,14 +1,13 @@
 import Navigation from "../Components/Navigation";
 import styles from "../page.module.css";
-import Form from "../Components/Form";
 
-export default function ContactPage() {
-    //This is where the form is called
+export default function AboutPage() {
+    //This is a page that tells a little bit about myself.
     return (
         <div className={styles.page}>
             <Navigation />
-            <h1>This is the Contact Page</h1>
-            <Form />
+            <h1>This is the About Page</h1>
+            <p>My name is Fahad Arif and I am currently a student at Humber College. This is the first assignment for the Advanced Front-End Programming Course(CPAN - 144).</p>
         </div>
     )
 }
